70a1ab20671b4948610cc82b73365c95e64902e8
Logeshk135 <Logeshk135@gmail.com>
2025-07-16 12:08:17 +0530

    search updated

--- index.js
+++ index.js
@@ -4,13 +4,33 @@ var sidenav = document.getElementById("sidenav")
 var MenuIcon = document.getElementById("MenuIcon")
 var closenav = document.getElementById("closenav")
 
-MenuIcon.addEventListener("click",function()
-{
-    sidenav.style.right=0
+MenuIcon.addEventListener("click", function () {
+    sidenav.style.right = 0
 })
 
-closenav.addEventListener("click" ,function()
-{
-    sidenav.style.right="-50%"
+closenav.addEventListener("click", function () {
+    sidenav.style.right = "-50%"
 })
 
+//product search funcationality
+var productContainer = document.getElementById("product-container")
+var search = document.getElementById("search")
+var productlist = productContainer.querySelectorAll("div")
+
+search.addEventListener("keyup",function(){
+    var enteredValue = event.target.Value.toUpperCase()
+
+    for(count = 0; count<productlist.length; count=count+1) 
+    {
+
+        var productname = productlist[count].querySelector("h1").textContent
+
+        if (productname.toUpperCase().indexOf(enteredValue)<0) 
+        {
+            productlist[count].style.display = "none"
+        }
+        else {
+            productlist[count].style.display = "block"
+        }
+    }
+})
